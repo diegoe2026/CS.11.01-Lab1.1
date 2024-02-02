4c7c3aa433840b966d0d4b319199d84a90a06854
diegoe2026 <157024760+diegoe2026@users.noreply.github.com>
2024-02-02 09:27:21 +0800

    Hi Ms Nahar

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter ur age");
+        int age = scan.nextInt();
+        int dateable = ( 7 + ( age / 2 ) );
+        System.out.println(dateable);
+        int page = (100 - age);
+                System.out.println("ur "+ age + " so read "+ page+" pages everyday ok?");
+
     }
 }
